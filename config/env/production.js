dc855a4325faa8523b0d2ca36f498158d4604c3e
Phạm Quôc Toàn <bossdiemmaimai@gmail.com>
2018-08-08 10:19:26 +0700

    Added my Sails.js project

--- config/env/production.js
+++ config/env/production.js
@@ -251,7 +251,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
-      'https://tranquil-forest-47832.herokuapp.com',
+      'https://tranquil-forest-47832.herokuapp.com'
     ],
 
 
