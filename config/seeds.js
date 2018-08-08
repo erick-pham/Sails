5714672cb105cf2bd62288303de4afc6cdc05200
Phạm Quôc Toàn <bossdiemmaimai@gmail.com>
2018-08-08 09:21:10 +0700

    Added my Sails.js project

---
+++ config/seeds.js
@@ -0,0 +1,49 @@
+/**
+ * Sails Seed Settings
+ * (sails.config.seeds)
+ *
+ * Configuration for the data seeding in Sails.
+ *
+ * For more information on configuration, check out:
+ * http://github.com/frostme/sails-seed
+ */
+var faker = require("faker");
+var sails = require("sails");
+module.exports.seeds = {
+    // seeding User
+    overwrite: true,
+    user: {
+      data: [
+        {
+          emailAddress: "admin@gmail.com",
+          fullName: "admin",
+          emailStatus: "confirmed",
+          isSuperAdmin: true,
+          password: "$2a$10$EPqg0D4LBeZpwBVUsUBeNe8VjmpbSliCGE9HGAypFeInUiSKRtubO"
+          //password: sails.helpers.passwords.hashPassword('admin@gmail.com')
+        }
+      ],
+      active: true
+    },
+
+  team: {
+    data: [
+      {
+        name: faker.internet.userName(),
+        createdAt: new Date()
+      }
+    ],
+    active: true
+  },
+
+  project: {
+    data: [
+      {
+        title: faker.internet.userName(),
+        startDate: new Date(),
+        endDate: new Date()
+      }
+    ],
+    active: true
+  },
+};
